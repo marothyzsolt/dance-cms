834894887ffe521d06bfa63abd4175c91f02885a
marothyzsolt@egaming.hu <Sherver1>
2019-04-20 02:49:45 +0200

    update

--- app/Http/Controllers/PoolController.php
+++ app/Http/Controllers/PoolController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 class PoolController extends Controller
 {
     public function index() {
+        $e = "";
+        die();
         while(true)
         {
-            if
+            if($this->hasUpdate())
+            {
+
+            }
             usleep(1000*1000);
         }
+    }
+
+    public function hasUpdate()
+    {
+        echo "123";
     }
 }
