d8094153c6ee274f3647383daa002a24d36e3e23
marothyzsolt@egaming.hu <Sherver1>
2019-04-20 02:31:35 +0200

    update

---
+++ app/Http/Controllers/PoolController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PoolController extends Controller
+{
+    public function index() {
+        while(true)
+        {
+            if
+            usleep(1000*1000);
+        }
+    }
+}
